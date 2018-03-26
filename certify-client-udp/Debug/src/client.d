27fcf2a0b86fc65b5bbd8236f6064b6c2095834c
KiritoLVL99 <37394806+KiritoLVL99@users.noreply.github.com>
2018-03-26 20:01:26 +0700

    update client, +a.out=assemble code

---
+++ certify-client-udp/Debug/src/client.d
@@ -0,0 +1,57 @@
+src/client.d: ../src/client.c ../src/../../noise/protocol.h \
+ ../src/../../noise/noise/protocol/constants.h \
+ ../src/../../noise/noise/protocol/errors.h \
+ ../src/../../noise/noise/protocol/names.h \
+ ../src/../../noise/noise/protocol/constants.h \
+ ../src/../../noise/noise/protocol/buffer.h \
+ ../src/../../noise/noise/protocol/cipherstate.h \
+ ../src/../../noise/noise/protocol/buffer.h \
+ ../src/../../noise/noise/protocol/hashstate.h \
+ ../src/../../noise/noise/protocol/dhstate.h \
+ ../src/../../noise/noise/protocol/signstate.h \
+ ../src/../../noise/noise/protocol/randstate.h \
+ ../src/../../noise/noise/protocol/symmetricstate.h \
+ ../src/../../noise/noise/protocol/cipherstate.h \
+ ../src/../../noise/noise/protocol/names.h \
+ ../src/../../noise/noise/protocol/handshakestate.h \
+ ../src/../../noise/noise/protocol/symmetricstate.h \
+ ../src/../../noise/noise/protocol/dhstate.h \
+ ../src/../../noise/noise/protocol/util.h
+
+../src/../../noise/protocol.h:
+
+../src/../../noise/noise/protocol/constants.h:
+
+../src/../../noise/noise/protocol/errors.h:
+
+../src/../../noise/noise/protocol/names.h:
+
+../src/../../noise/noise/protocol/constants.h:
+
+../src/../../noise/noise/protocol/buffer.h:
+
+../src/../../noise/noise/protocol/cipherstate.h:
+
+../src/../../noise/noise/protocol/buffer.h:
+
+../src/../../noise/noise/protocol/hashstate.h:
+
+../src/../../noise/noise/protocol/dhstate.h:
+
+../src/../../noise/noise/protocol/signstate.h:
+
+../src/../../noise/noise/protocol/randstate.h:
+
+../src/../../noise/noise/protocol/symmetricstate.h:
+
+../src/../../noise/noise/protocol/cipherstate.h:
+
+../src/../../noise/noise/protocol/names.h:
+
+../src/../../noise/noise/protocol/handshakestate.h:
+
+../src/../../noise/noise/protocol/symmetricstate.h:
+
+../src/../../noise/noise/protocol/dhstate.h:
+
+../src/../../noise/noise/protocol/util.h:
